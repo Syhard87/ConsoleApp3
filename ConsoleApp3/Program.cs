23d41a5f16f95e2bd911b44e1ae17cb307c1f1bf
Antoine <Antoine@LAPTOP-5UM8PG3I>
2021-10-20 15:03:54 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            Console.Write("Entrez votre age :");
+            age = int.Parse(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.Write("Vous etes majeur ");
+            }
+            else
+            {
+                Console.Write($"Vous etes majeur dans " + (18 - age) + " ans ");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
